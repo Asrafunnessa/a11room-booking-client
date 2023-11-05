f552d1d6c7ea5e61b3809aa607f2dec7f0456e31
Asrafunnessa <ayeshapriya0@gmail.com>
2023-11-05 10:43:55 +0600

    edit newslwtter

--- src/pages/Home/Newsletter/Newsletter.jsx
+++ src/pages/Home/Newsletter/Newsletter.jsx
@@ -1,5 +1,6 @@
-import img1 from '../../../assets/images/news/S1.jpeg';
-import img2 from '../../../assets/images/news/STU6.jpeg';
+import img1 from '../../../assets/images/banner/4.jpeg';
+import img2 from '../../../assets/images/banner/5.jpg';
+
 
 const Newsletter = () => {
     return (
@@ -10,11 +11,11 @@ const Newsletter = () => {
                 <img src={img2} className="w-1/2 absolute right-5 top-1/2 rounded-lg border-8 border-white shadow-2xl" />
                 </div>
                 <div className='lg:w-1/2 space-y-5 p-4'>
-                    <h3 className='text-3xl text-orange-500 font-bold'>About Us</h3>
-                    <h1 className="text-5xl font-bold">We Are Qualified & of experience in this field</h1>
-                    <p className="py-6">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.</p>
-                    <p className='py-6'>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.</p>
-                    <button className="btn btn-warning">Get More Info</button>
+                    <h3 className='text-2xl font-bold'>SUBSCRIPTION</h3>
+                    <h1 className="text-5xl text-orange-500 font-bold">Newsletter</h1>
+                    <p className="py-6">Class apent taciti sociosqu ad lent per conu bia nostraellus. praesent eget em ante.Class apent taciti sociosqu ad lent per conu bia nostraellus. praesent eget em ante.Class apent taciti sociosqu ad lent per conu bia nostraellus. praesent eget em ante.</p>
+                    <p className='py-6'>Class apent taciti sociosqu ad lent per conu bia nostraellus.praesent eget em ante.</p>
+                    <button className="btn btn-warning">Send Email</button>
                 </div>
             </div>
         </div>
