69f0ae64b5024399f31925b4db71d44ac9d3b1d0
Asrafunnessa <ayeshapriya0@gmail.com>
2023-11-05 10:17:54 +0600

    Rooms data load

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import { useEffect } from "react";
+// import RoomsCard from "./RoomsCard";
 
 const Rooms = () => {
+
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        fetch('rooms.json')
+            .then(res => res.json())
+            .then(data => setRooms(data));
+    }, [])
+
     return (
         <div className="mt-4">
             <div className="text-center">
@@ -7,14 +19,15 @@ const Rooms = () => {
                 <h2 className="text-5xl">Explore Our Rooms</h2>
                 {/* <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p> */}
             </div>
-            {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {
-                    services.map(service => <ServicesCard
-                        key={service._id}
-                        service={service}
-                    ></ServicesCard>)
-                }
-            </div> */}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {/* {
+                    rooms.map(room => <RoomsCard
+                        key={room._id}
+                        room={room}
+                    ></RoomsCard>)
+                } */}
+                <p>Rooms: {rooms.length}</p>
+            </div>
         </div>
     );
 };
---
+++ src/pages/Rooms/RoomsCard.jsx
@@ -0,0 +1,28 @@
+// /* eslint-disable react/prop-types */
+// import { Link } from "react-router-dom";
+
+
+// const RoomsCard = ({ room }) => {
+//     const { _id, room_type, img, price_per_night } = room;
+//     return (
+//         <div className="card w-96 bg-base-100 shadow-xl">
+//             <figure className="px-10 pt-10">
+//                 <img src={img} alt="Shoes" className="rounded-xl" />
+//             </figure>
+//             <div className="card-body">
+//                 <h2 className="card-title">{room_type}</h2>
+//                 <p className="text-xl text-orange-500">Price: ${price_per_night}</p>
+//                 <div className="card-actions">
+//                     {/* <Link to={`/book/${_id}`}>
+//                         <button className="btn btn-primary">Book Now</button>
+//                     </Link> */}
+//                     <Link to={`/roomsDetails/${_id}`}>
+//                         <button className="btn btn-primary">Details</button>
+//                     </Link>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default RoomsCard;
