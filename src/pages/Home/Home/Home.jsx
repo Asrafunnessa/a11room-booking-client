9bdea24bceca0a35f79b846f5ed5d2e50d1148c9
Asrafunnessa <ayeshapriya0@gmail.com>
2023-11-05 10:39:02 +0600

    create json data

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -10,3 +10,23 @@ const Home = () => {
 };
 
 export default Home;
+
+
+// https://i.ibb.co/SNf7WY0/D1.jpg
+// https://i.ibb.co/7rGKPd1/D2.png
+// https://i.ibb.co/2ZKGhJS/D3.jpg
+// https://i.ibb.co/M5Cbk6j/D4.jpg
+// https://i.ibb.co/P9PQdJp/D5.jpg
+// https://i.ibb.co/0FnC8tn/D6.jpg
+// https://i.ibb.co/xHMgqHG/S1.jpg
+// https://i.ibb.co/c1sW30Z/S2.jpg
+// https://i.ibb.co/LCsTySk/S3.jpg
+// https://i.ibb.co/SVy6VxQ/S4.jpg
+// https://i.ibb.co/f2d585c/S5.jpg
+// https://i.ibb.co/c1vMKsJ/S6.jpg
+// https://i.ibb.co/VJY3Pq4/STU1.jpg
+// https://i.ibb.co/Wf0Q4Yf/STU2.jpg
+// https://i.ibb.co/8zP6K3m/STU3.jpg
+// https://i.ibb.co/2sM8sT5/STU4.jpg
+// https://i.ibb.co/qJpmTyy/STU5.jpg
+// https://i.ibb.co/TkctdRv/STU6.jpg
