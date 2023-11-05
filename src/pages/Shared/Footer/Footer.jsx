28e939930ca5cd05b45ef97e460dac92a6ba625e
Asrafunnessa <ayeshapriya0@gmail.com>
2023-11-05 10:54:16 +0600

    set  navbar

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import logo from '../../../assets/hotello_logo.svg'
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
+        <footer className="footer p-10 bg-gray-400 text-base-content">
             <aside>
                 <img src={logo} alt="" />
                 <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
