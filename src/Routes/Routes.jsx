f1b4856800f74801abb2248452fa02c46dde6e7d
Asrafunnessa <ayeshapriya0@gmail.com>
2023-11-09 09:49:09 +0600

    load user specific data

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,7 +6,7 @@ import Login from './../pages/Login/Login';
 import SignUp from "../pages/SignUp/SignUp";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import EachRoom from "../pages/EachRoomDetails/EachRoom";
-import ConfirmBooking from "../pages/ConfirmBooking/ConfirmBooking";
+import Bookings from "../pages/Bookings/Bookings";
 
 const router = createBrowserRouter([
     {
@@ -36,9 +36,8 @@ const router = createBrowserRouter([
           loader: ({params}) => fetch(`http://localhost:5000/rooms/${params.id}`)
         },
         {
-          path: 'confirmBooking/:id',
-          element: <ConfirmBooking></ConfirmBooking>,
-          loader: ({params}) => fetch(`http://localhost:5000/rooms/${params.id}`)
+          path: 'bookings',
+          element: <Bookings></Bookings>
         }
       ]
     },
