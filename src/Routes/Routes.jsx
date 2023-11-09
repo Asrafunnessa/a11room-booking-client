ba4dc5d5dd9b104e0693fdab73844594cfe90422
Asrafunnessa <ayeshapriya0@gmail.com>
2023-11-09 09:46:24 +0600

    Rooms booking data get successfully

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,7 @@ import Login from './../pages/Login/Login';
 import SignUp from "../pages/SignUp/SignUp";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import EachRoom from "../pages/EachRoomDetails/EachRoom";
+import ConfirmBooking from "../pages/ConfirmBooking/ConfirmBooking";
 
 const router = createBrowserRouter([
     {
@@ -32,6 +33,11 @@ const router = createBrowserRouter([
         {
           path: 'eachRoomDetails/:id',
           element: <EachRoom></EachRoom>,
+          loader: ({params}) => fetch(`http://localhost:5000/rooms/${params.id}`)
+        },
+        {
+          path: 'confirmBooking/:id',
+          element: <ConfirmBooking></ConfirmBooking>,
           loader: ({params}) => fetch(`http://localhost:5000/rooms/${params.id}`)
         }
       ]
--- src/pages/EachRoomDetails/EachRoom.jsx
+++ src/pages/EachRoomDetails/EachRoom.jsx
@@ -1,37 +1,84 @@
 /* eslint-disable no-unused-vars */
 import { useLoaderData } from "react-router-dom";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+import { useContext } from "react";
 
 const EachRoom = () => {
 
     const roomDetails = useLoaderData();
+    const {user} = useContext(AuthContext);
 
     const { _id, room_type, price_per_night, room_size, special_offers, description, img } = roomDetails;
 
+    const handleRoomBook = event =>{
+        event.preventDefault();
+
+        const form = event.target;
+        const email = user?.email;
+        const date = form.date.value;
+        const booking = {
+             room_type,
+             img,
+             email,
+             date,
+             _id,
+             price_per_night
+        }
+        console.log(booking);
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+               'content-type' : 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.insertedId){
+                alert('Room book successfully')
+            }
+        })
+    }
+
+  
+  
     return (
-        <div className="card card-side bg-base-100 shadow-xl mt-4">
-        <figure><img src={img} alt="Movie" /></figure>
-        <div className="flex justify-between w-full p-4">
-            <div>
-                <h2 className="card-title text-3xl"><span className="text-orange-600 font-bold">Name:</span> {room_type}</h2>
-                <p className="text-2xl my-4"><span className="text-orange-600 font-bold">Price:</span> ${price_per_night}</p>
-                <p className="text-2xl my-4"><span className="text-orange-600 font-bold">Room Size:</span> {room_size}</p>
-                <p className="text-2xl my-4"><span className="text-orange-600 font-bold">Availability:</span> Its available. </p>
-                <p className="text-2xl my-4"><span className="text-orange-600 font-bold">Description:</span> {description}</p>
-                <p className="text-2xl my-4"><span className="text-orange-600 font-bold">Special Offer:</span> {special_offers}</p>
-            </div>
-            <div className="card-actions justify-end">
-                <div className="btn-group btn-group-vertical space-y-4">
-                    <Link to={`roomBooked/${_id}`}>
-                    <button className="btn btn-primary">Book Now</button>
-                    </Link>
-                    {/* <button
+        <form   onSubmit={handleRoomBook}  >
+            <div className="card card-side bg-base-100 shadow-xl mt-4">
+            <figure><img src={img} alt="Movie" /></figure>
+            <div className="flex justify-between w-full p-4">
+                <div>
+                    <h2 className="card-title text-3xl"><span className="text-orange-600 font-bold">Name:</span> {room_type}</h2>
+                    <p className="text-2xl my-4"><span className="text-orange-600 font-bold">Price:</span> ${price_per_night}</p>
+                    <p className="text-2xl my-4"><span className="text-orange-600 font-bold">Room Size:</span> {room_size}</p>
+                    <p className="text-2xl my-4"><span className="text-orange-600 font-bold">Availability:</span>available. </p>
+                    <p className="text-2xl my-4"><span className="text-orange-600 font-bold">Description:</span> {description}</p>
+                    <p className="text-2xl my-4"><span className="text-orange-600 font-bold">Special Offer:</span> {special_offers}</p>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text text-2xl text-orange-600 font-bold">Date</span>
+                        </label>
+                        <input type="date" name="date" className="input input-bordered" />
+                    </div>
+                    <div className="card-actions justify-end my-4">
+                        <div className="btn-group btn-group-vertical space-y-4">
+                            {/* <Link to={`confirmBooking/${_id}`}>
+                                <button className="btn btn-warning btn-outline">Book Now</button>
+                            </Link> */}
+                            {/* <button
                         onClick={() => handleDelete(_id)}
                         className="btn bg-orange-500">X</button> */}
+                        <div className="form-control mt-6">
+                    <input className="btn btn-warning btn-block btn-outline" type="submit" value="Book Now" />
+                </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+        </form>
 
     );
 };
@@ -39,3 +86,4 @@ const EachRoom = () => {
 
 export default EachRoom;
 
+ 
